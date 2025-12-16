b8eb103171ef0ea694aab700f5340ca86a6c98ae
Marc <marccuentas95@gmail.com>
2025-12-16 19:53:47 +0100

    test

---  middleware.py
+++  middleware.py
@@ -1,9 +1,8 @@
-class DisableCSRFOnAPI:
+# eclipse/middleware.py
+class DummyMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path.startswith("/api/"):
-            setattr(request, "_dont_enforce_csrf_checks", True)
         response = self.get_response(request)
         return response
