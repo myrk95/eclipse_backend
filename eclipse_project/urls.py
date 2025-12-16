8514d186acfcd44925f6315b299daf8d319247bd
Marc <marccuentas95@gmail.com>
2025-12-16 17:33:11 +0100

    test

--- eclipse_project/urls.py
+++ eclipse_project/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('tu_app.urls')),  # <- Ajusta con el nombre de tu app
+    path('api/v1/', include('eclipse.urls')),  # Aquí incluyes las urls de tu app "eclipse"
 ]
 
 if settings.DEBUG:
