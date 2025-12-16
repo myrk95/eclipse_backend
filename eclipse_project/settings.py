5442882f83287ae2176f9f87ffcece5c5142ca61
Marc <marccuentas95@gmail.com>
2025-12-16 19:16:24 +0100

    t

--- eclipse_project/settings.py
+++ eclipse_project/settings.py
@@ -30,7 +30,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'eclipse_project.middleware.DisableCSRFOnAPI',  # <- Middleware para desactivar CSRF en /api/
+    'eclipse.middleware.DisableCSRFOnAPI',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
