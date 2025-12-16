874042853a8061cccaa8882be56f4d299e6df4ae
Marc <marccuentas95@gmail.com>
2025-12-16 19:55:42 +0100

    tt

--- eclipse_project/wsgi.py
+++ eclipse_project/wsgi.py
@@ -1,13 +1,12 @@
 """
 WSGI config for eclipse_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Añade la carpeta raíz del proyecto al PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
